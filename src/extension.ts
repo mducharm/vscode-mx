6ed1b48d8082caadca109813db92fa6ddca7bc47
Michael Ducharm <MichaelD@zoeller.com>
2019-12-16 15:33:12 -0500

    added cmd for generating js classes

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,62 @@
 import * as vscode from 'vscode';
+import { Command } from './types';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Extension "mx" is now active.');
 
-	let commands = [
+	let commands: Command[] = [
 		{
 			name: 'extension.helloWorld',
 			command: () => {
 				vscode.window.showInformationMessage('Hello World!');
 			}
-		}
-	]
+		},
+		{
+			name: 'extension.makeJSClass',
+			command: () => {
+				const activeEditor = vscode.window.activeTextEditor;
+				if (activeEditor) {
+					let currentLine = activeEditor.document.lineAt(activeEditor.selection.active.line);
+					let lineText = currentLine.text;
+					let [className, ...props] = lineText.split(' ').filter((v => v.length > 0));
 
-	let disposables = commands.map(cmd => vscode.commands.registerCommand(cmd.name, cmd.command));
+					let params = props.reduce((a, p, ind, arr) => {
+						if (arr.length === 1) { return p; }
+						else if (ind === arr.length - 1) { return a + p; }
+
+						return a + p + ", ";
+					}, "");
+
+
+					let propDeclarations = props.map(p => {
+						return `\t${p};\n`;
+					})
+
+					let propAssignments = props.map(p => {
+						return `\tthis.${p} = ${p};\n`;
+					})
+
+					let classText = [
+						`class ${className}` + " {\n",
+						...propDeclarations,
+						"\n",
+						`\tconstructor(${params}) {\n`,
+						...propAssignments,
+						"\t}\n",
+						"}"
+					].reduce((a, v) => a + v)
+
+					activeEditor.edit((editBuilder) => {
+						editBuilder.replace(currentLine.range, classText);
+					})
+
+				}
+			}
+		}
+	];
+
+	let disposables = commands.map((cmd: Command) => vscode.commands.registerCommand(cmd.name, cmd.command));
 
 	disposables.forEach(disposable => context.subscriptions.push(disposable));
 
